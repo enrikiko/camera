2ae391e07f5590a5d7f87dfe772560dba0e9d1fb
Enrique Ramos <enrikiko_91@hotmail.com>
2024-09-17 01:00:39 +0200

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 dir="home/miso/Desktop/camera"
+cd $dir
 set -a
-. $dir/.env
+. .env
 set +a
 echo $CAMERA_KEY
 echo $dir
 while true; 
   do
-  cd $dir
+  
   git pull
-  . $dir/install.sh
+  . install.sh
   for i in `seq 1 2`
   do 
-    python $dir/send.py
+    python send.py
     sleep 1
   done
 done
