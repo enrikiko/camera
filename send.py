8844a5a3b53b4827474dfa804bbf887441a8abbc
Enrique Ramos <enrikiko_91@hotmail.com>
2024-09-17 01:05:12 +0200

    Update send.py

--- send.py
+++ send.py
@@ -10,12 +10,12 @@ while True:
         time.sleep(1) 
         return_value,image = camera.read()    
         image_rotated = cv2.rotate(image, cv2.ROTATE_180)
-        cv2.imwrite('test222.jpg',image)    
+        cv2.imwrite('/tmp/pic.jpg',image)    
         secret = False
 
     while secret==False:
         url = 'https://cortijo-security-cameras-dev.cortijodemazas.com/update'
-        with open('test222.jpg', 'rb') as file:    
+        with open('/tmp/pic.jpg', 'rb') as file:    
             binary_data = file.read()    
             headers = {'tenant': 'tenant1', 'Content-Type': 'image/jpeg', 'camera' : 'camera1', 'x-api-key': os.environ['CAMERA_KEY'], 'Content-Length': str(len(binary_data))}    
         response = requests.post(url, data=binary_data, headers=headers)    
